2cefae1397aea0d6c1526c432c1cb1037ce0efa9
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-30 16:56:53 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a parrallelogram.
+ */
+function calculate () {
+  // input
+  const a = parseInt(document.getElementById('base-a-of-trapezoid').value)
+  const b = parseInt(document.getElementById('base-b-of-trapezoid').value)    
+  const height = parseInt(document.getElementById('height-of-trapezoid').value)
+
+  // process
+  const area = [ ( a + b ) / 2 * height ]
+
+  // output
+  document.getElementById('answers').innerHTML = 'Area is: ' + area.toFixed(2) + ' cm²'
+}
